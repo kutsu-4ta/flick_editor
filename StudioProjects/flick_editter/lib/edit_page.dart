52ef46191e86d1510030f0203d45225e7c5e36ac
kutsu-4ta <yamashita.bus@gmail.com>
2021-12-02 00:19:22 +0900

    chore:EditPage作成、遷移確認済み

---
+++ StudioProjects/flick_editter/lib/edit_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EditPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('エディタページです'),
+      ),
+      body: Container(
+        color: Colors.red,
+      ),
+    );
+  }
+}
--- StudioProjects/flick_editter/lib/main.dart
+++ StudioProjects/flick_editter/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'edit_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -52,14 +53,18 @@ class _MyHomePageState extends State<MyHomePage> {
   /// @param    void
   /// @return   void
   void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+    // setState(() {
+    //   // This call to setState tells the Flutter framework that something has
+    //   // changed in this State, which causes it to rerun the build method below
+    //   // so that the display can reflect the updated values. If we changed
+    //   // _counter without calling setState(), then the build method would not be
+    //   // called again, and so nothing would appear to happen.
+    //   _counter++;
+
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => EditPage()),
+    );
   }
 
   @override
